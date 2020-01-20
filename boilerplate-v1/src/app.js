259f7adb3dcb975fc6601cba347ea1eed3d1587b
Sergei Komarov <komarovs33@mail.ru>
2020-01-20 13:12:08 +0300

    add routes

---
+++ boilerplate-v1/src/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import IndecisionApp from "./components/IndecisionApp";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+ReactDOM.render(<p>This is my boiler plate</p>, document.getElementById("app"));
--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,7 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import IndecisionApp from "./components/IndecisionApp";
+import { BrowserRouter, Route } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<p>This is my boiler plate</p>, document.getElementById("app"));
+const ExpenseDashboardPage = () => (
+  <div>This is from my dashboard component</div>
+);
+
+const AddExpensePage = () => <div>AddExpensePage</div>;
+
+const EditExpensePage = () => <div>EditExpensePage</div>;
+
+const HelpExpensePage = () => <div>HelpExpensePage</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpExpensePage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
