25427b243f68b001c571ae87d602a4587e99bcf0
Sergei Komarov <komarovs33@mail.ru>
2020-01-20 21:44:37 +0300

    put components to separate files

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,56 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+
+import AppRouter from "./routers/AppRouter";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-const ExpenseDashboardPage = () => (
-  <div>This is from my dashboard component</div>
-);
-
-const AddExpensePage = () => <div>AddExpensePage</div>;
-
-const EditExpensePage = () => <div>EditExpensePage</div>;
-
-const HelpExpensePage = () => <div>HelpExpensePage</div>;
-
-const NotFoundPage = () => (
-  <div>
-    404 - <Link to="/">go home</Link>
-  </div>
-);
-
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>
-      Dashboard
-    </NavLink>
-    <NavLink to="/create" activeClassName="is-active">
-      Create{" "}
-    </NavLink>
-    <NavLink to="/edit" activeClassName="is-active">
-      Edit{" "}
-    </NavLink>
-    <NavLink to="/help" activeClassName="is-active">
-      Help
-    </NavLink>
-  </header>
-);
-
-const routes = (
-  <BrowserRouter>
-    <div>
-      <Header />
-      <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage} />
-        <Route path="/help" component={HelpExpensePage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById("app"));
+ReactDOM.render(<AppRouter />, document.getElementById("app"));
---
+++ expensify-app/src/components/HelpExpensePage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const HelpExpensePage = () => <div>HelpExpensePage</div>;
+
+export default HelpExpensePage;
