d5b93f008291849898bb2cc367932a4d1d756126
Sergei Komarov <komarovs33@mail.ru>
2020-01-24 18:49:46 +0300

    add count actions

--- expensify-app/src/playground/redux101.js
+++ expensify-app/src/playground/redux101.js
@@ -3,10 +3,13 @@ import { createStore } from "redux";
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
+      const incrementBy =
+        typeof action.incrementBy === "number" ? action.incrementBy : 1;
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       };
     case "DECREMENT":
+      const decrementBy = action.decrementBy ? action.decrementBy : 1;
       return {
         count: state.count - 1
       };
@@ -14,23 +17,36 @@ const store = createStore((state = { count: 0 }, action) => {
       return {
         count: 0
       };
+    case "SET":
+      return {
+        count: action.count
+      };
     default:
       return state;
   }
 });
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
 
 store.dispatch({
-  type: "INCREMENT"
+  type: "INCREMENT",
+  incrementBy: 5
 });
+
+// unsubscribe();
 
 store.dispatch({
   type: "RESET"
 });
 
 store.dispatch({
-  type: "DECREMENT"
+  type: "DECREMENT",
+  decrementBy: 10
 });
 
-console.log(store.getState());
+store.dispatch({
+  type: "SET",
+  count: 101
+});
