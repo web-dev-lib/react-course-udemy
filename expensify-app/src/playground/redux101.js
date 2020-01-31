1f96330ccaa1e1a519708d22da3899bdf23e8929
Sergei Komarov <komarovs33@mail.ru>
2020-01-31 18:17:24 +0300

    add redux-expensify example

--- expensify-app/src/playground/redux101.js
+++ expensify-app/src/playground/redux101.js
@@ -21,7 +21,11 @@ const resetCount = () => ({
 });
 // <--
 
-const store = createStore((state = { count: 0 }, action) => {
+// Reducers
+// 1 Reducers are pure functions
+// 2 Never change state or action
+
+const countReducer = (state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
       const incrementBy =
@@ -45,7 +49,9 @@ const store = createStore((state = { count: 0 }, action) => {
     default:
       return state;
   }
-});
+};
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
