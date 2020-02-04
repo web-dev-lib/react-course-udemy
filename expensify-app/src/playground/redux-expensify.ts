d85fefaace754f4f7b3c83fbb19764a185be56a8
Sergei Komarov <komarovs33@mail.ru>
2020-02-04 14:28:11 +0300

    finish store

--- expensify-app/src/playground/redux-expensify.ts
+++ expensify-app/src/playground/redux-expensify.ts
@@ -121,6 +121,23 @@ const filtersReducer = (state = filterReducerDefaultState, {type, text = '', sor
   }
 };
 
+// get visible expenses
+const getVisibleExpenses = (expenses, {text, sortBy, startDate, endDate}) => {
+  return expenses.filter((expense) => {
+    const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+    const endDatematch = typeof endDate !== 'number' || expense.endDate >= endDate;
+    const textMatch = expense.description.toLowerCase().includes(text);
+
+    return startDateMatch && endDatematch && textMatch;
+  }).sort((a, b) => {
+    if(sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1 : -1
+    } else if (sortBy === 'amount') {
+      return a.amount < b.amount ? 1 : -1
+    }
+  })
+}
+
 // Store creatiion
 const store = createStore(
   combineReducers({
@@ -130,25 +147,27 @@ const store = createStore(
 );
 
 store.subscribe(() => {
-  console.log(store.getState());
+  const state = store.getState()
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+  console.log(visibleExpenses);
 });
 
-// const expenseOne = store.dispatch(
-//   addExpense({ description: "Rent", amount: 1000 })
-// );
-// const expenseTwo = store.dispatch(
-//   addExpense({ description: "2Rent", amount: 2000 })
-// );
+const expenseOne = store.dispatch(
+  addExpense({ description: "Rent", amount: 1000, createdAt: -1000 })
+);
+const expenseTwo = store.dispatch(
+  addExpense({ description: "2Rent", amount: 2000, createdAt: 1000 })
+);
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 // store.dispatch(setTextFilter('rent4'))
 // store.dispatch(setTextFilter())
-// store.dispatch(sortByAmount())
+store.dispatch(sortByAmount())
 // store.dispatch(sortByDate())
 
-store.dispatch(setStartDate(125))
 store.dispatch(setStartDate())
-store.dispatch(setEndDate(1230))
+// store.dispatch(setStartDate())
+// store.dispatch(setEndDate(1230))
 
 const demoState = {
   expenses: [
